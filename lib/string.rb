08338af982f9ff4a47e34211b8465b5aa88d2d75
Nell Shamrell <nshamrell@bluebox.net>
2012-04-03 20:06:21 -0700

    initial solution

--- lib/string.rb
+++ lib/string.rb
@@ -1,6 +1,35 @@
 
 class String
   def empty?
+#    strip.length == 0
+#     strip.count? > 0
+    strip == ""
+
+
+    # If character count > 0
+
+    # Check for letters, numbers, all the combinations...
+
+    # Check for end of line
+
+    # empty string = "\n"
+
+    # Operators!!!!
+    # != ""
+
+    # scanf
+  end
+
+  def empty_1?
     strip.length == 0
+#    strip.count? > 0
+  end
+
+  def empty_2?
+
+  end
+
+  def empty_3?
+
   end
 end
